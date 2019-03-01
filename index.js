1d64bd4712a15bd9d67617615380e8aed0363ee4
Merline Belizaire <merline@noukod.com>
2019-03-01 17:06:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,81 +1,61 @@
+let store = { drivers: [], passengers: [], trips: [] };
 
 let driverId = 0;
-
-let store = {drivers: [], passengers: [], trips: []};
+let passengerId = 0;
+let tripId = 0;
 
 class Driver {
   constructor(name) {
-    this.id = ++driverId
-    this.name = name
-
-    store.drivers.push(this)
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
-
   trips() {
     return store.trips.filter(trip => {
-      return trip.driverId === this.id
-    })
+      return trip.driverId == this.id;
+    });
   }
-
   passengers() {
     return this.trips().map(trip => {
-      return trip.passenger()
-    })
+      return trip.passenger();
+    });
   }
-
 }
 
-let passengerId = 0;
-
 class Passenger {
-  constructor(name, driver) {
-    this.id = ++passengerId;
-    this.name = name
-    if (driver){
-      this.driverId = driver.id
-    }
-
-    store.passengers.push(this)
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
   }
 
   trips() {
     return store.trips.filter(trip => {
-      return trip.passengerId === this.id
-    })
+      return trip.passengerId == this.id;
+    });
   }
-
   drivers() {
     return this.trips().map(trip => {
-      return trip.driver()
-    })
+      return trip.driver();
+    });
   }
 }
-
-let tripId = 0;
 
 class Trip {
   constructor(driver, passenger) {
-    this.id = ++tripId
-    this.driverId = driver.id
-    this.passengerId = passenger.id
-
-    store.trips.push(this)
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
   }
-
-  setPassenger(passenger) {
-    this.passengerId = passenger.id
-  }
-
-  passenger() {
-    return store.passengers.find(passenger => {
-      return passenger.id === this.passengerId
-    });
-  }
-
   driver() {
     return store.drivers.find(driver => {
-      return driver.id === this.driverId
-      })
-
-    }
- }
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
